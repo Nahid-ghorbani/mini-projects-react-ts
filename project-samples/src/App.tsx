e8b1379a3f433e880cdeff8d30b194825a68309f
Nahid <ghorbani.nca@gmail.com>
2023-08-04 17:45:44 +0330

    event examples

--- project-samples/src/App.tsx
+++ project-samples/src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
 function App() {
+  const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.defaultPrevented;
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  const clickHandler = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.altKey;
+  };
+
   return (
     <>
-      <div>Hello world</div>
+      <form onSubmit={submitHandler}>
+        <input type="text" onChange={changeHandler} />
+        <button onClick={clickHandler}>Click</button>
+      </form>
     </>
   );
 }
